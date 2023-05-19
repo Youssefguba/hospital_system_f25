62c577c846d53949972307df72a63b48113d45cd
Youssefguba <Youssef.guba@abwaab.me>
2023-05-19 11:41:52 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/hospital/f25/hospital_system_f25/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hospital.f25.hospital_system_f25
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+
+// Structure
+// Core Values [ Assets / Colors ]
+// Start from first screen
