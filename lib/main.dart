094b29c658e0266858796964f60441126de60258
Youssefguba <Youssef.guba@abwaab.me>
2023-05-19 11:46:19 +0300

    add .gitkeep files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp()); // Conflict
+}
 
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
 
 
